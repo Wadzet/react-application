9fc85ed0875629204d94335732d12b8749240a24
Wadzet <vadymzastavniy@gmail.com>
2022-12-23 21:28:49 +0200

    lab3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import './App.css';
-import Navigation from './components/navigation';
-import Content from './components/content';
 
-function App () {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
-
-export default App;
